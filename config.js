4df0d13b89f938e4e223d6ab2c3edbfa58d4445f
Dorcas Kareithi <19709060+DeeKareithi@users.noreply.github.com>
2020-02-16 05:31:23 +0300

    upate 3

--- config.js
+++ config.js
@@ -5,39 +5,55 @@ var config = {
     theme: 'dark',
     alignment: 'right',
     title: 'Access to Healthcare after an Accident in Lagos - CcHUB Design Lab Showcase',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+	    subtitle: 'How easy it to access healthcare after getting an accident in Lagos?',
     byline: 'By Dorcas Kareithi and Steven Karera',
-    footer: 'Source: source of data- https://grid3.gov.ng/datasets.',
+    footer: 'Data Source: WorldBank Data, Nigeria Watch data and Grid3 Data <a href https://grid3.gov.ng/datasets. /a>',
     chapters: [
         {
-            id: 'slug-style-id',
+            id: 'Lagos Showcase Map',
             title: 'Road Accidents in Lagos (2016 to 2020)',
-            image: './path/to/image/source.png',
+            image: 'https://github.com/DeeKareithi/DeeKareithi.github.io/blob/master/images/Road%20Accidents%20Lagos%20(2006-2019).png?raw=true',
+
             description: 'Over the past 4 years, road accidents in Lagos have been on the increase. THis has subsequently led to an increase in deaths in the State, as shown below.',
+			
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 13.5,
+                center: [3.4, 6.4],
+                zoom: 8.5,
                 pitch: 60,
                 bearing: 0
             },
             onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1
-                // }
+                {
+                    layer: 'nigeria_layer',
+                    opacity: 1
+                }
             ],
             onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
+                 {
+                     layer: 'lagos_boundaries',
+                     opacity: 0.25
+                }
             ]
         },
         {
             id: 'other-identifier',
-            title: 'Health Facilities in Nigeria',
-            image: './path/to/image/source.png',
+            title: 'Health Facilities in Lagos',
+            image: '',
             description: 'After getting an accident in Lagos, we ask ourselves, what are the closest health facilities that those injured can access? What Options do they have?',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 13.5,
+                pitch: 60,
+                bearing: -43.2
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'other-identifier',
+            title: 'Traffic in Lagos',
+            image: '',
+            description: 'The accident victim needs to be taken to the nearest health facility to recieve firts aid. What is the fastest way to get there? Which routes should they avoid?',
             location: {
                 center: [-77.020636, 38.886900],
                 zoom: 13.5,
