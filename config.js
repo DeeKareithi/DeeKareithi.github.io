5399997460c549910a0febe8b8582bef8be41fba
Dorcas Kareithi <19709060+DeeKareithi@users.noreply.github.com>
2020-02-18 12:10:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    style: 'mapbox://styles/dorckar/ck6nyrdnk1fvv1jn3ryegowfb',
-    accessToken: 'sk.eyJ1IjoiZG9yY2thciIsImEiOiJjazZwdDJoNmIxN3FkM2xseXFtOGhuMHpoIn0.QOonnNPUEQCYvI_D2DQZ7A',
+    style: 'mapbox://styles/dorckar/ck6o1vd1n15os1jlbdt6voyri',
+    accessToken: 'pk.eyJ1IjoiZG9yY2thciIsImEiOiJjazZua21jaG4wdjk2M3RsaWp0dGVvdzAxIn0._WwBVT5xalfRp284lwtiEQ',
     showMarkers: false,
     theme: 'dark',
     alignment: 'right',
